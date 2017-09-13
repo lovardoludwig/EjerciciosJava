0445a2b82492fc68f3629a5e641e3c10a88a1aec
lovardoludwig <31898651+lovardoludwig@users.noreply.github.com>
2017-09-13 16:02:35 -0300

    primer commit
    hicimos el enunciado

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     * Se debe pedir un nombre y se debe mostrar el mismo nombre
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
