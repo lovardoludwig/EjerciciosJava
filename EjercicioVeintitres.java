b26180ff2b426826a623641c4c867ffcf9c98744
lovardoludwig <31898651+lovardoludwig@users.noreply.github.com>
2017-09-23 02:23:57 -0300

    primer commit
    Debemos pedir el ancho y el largo de un terreno e informar cuantos metros de alambre necesitamos para dar tres (3) vueltas de alambre

---
+++ EjercicioVeintitres.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioveintitres;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Agus
+ */
+public class EjercicioVeintitres {
+
+    /**
+     * @param args the command line arguments
+     * Debemos pedir el ancho y el largo de un terreno e informar cuantos metros de alambre necesitamos para dar tres (3) vueltas de alambre
+     */
+    public static void main(String[] args) {
+        Scanner MiEscaner = new Scanner(System.in);
+        Double Ancho;
+        Double Largo;
+        
+        
+        
+        
+        
+        
+    }
+    
+}
