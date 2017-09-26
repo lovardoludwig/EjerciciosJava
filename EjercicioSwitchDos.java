29be85e67c203f0942ad0ad0f6bf94da3bc5acf3
lovardoludwig <31898651+lovardoludwig@users.noreply.github.com>
2017-09-26 17:56:36 -0300

    primer commit
    ejercicio incompleto

---
+++ EjercicioSwitchDos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioswitchdos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioSwitchDos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner MiEscaner  = new Scanner(System.in);
+        Integer MesDelAño;
+        String DatoAux;
+        
+        DatoAux = MiEscaner.next();
+        MesDelAño = Integer.parseInt(DatoAux);
+        
+        switch(MesDelAño)
+        {
+            case 1
+        }
+    }
+    
+}
