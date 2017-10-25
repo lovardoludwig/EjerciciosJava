def0c586be4a14d550adbbc5ad58c359aa9cf603
lovardoludwig <31898651+lovardoludwig@users.noreply.github.com>
2017-10-25 17:41:04 -0300

    primer commit
    aprendemos DoWhile

---
+++ EjemploDoWhile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplodowhile;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjemploDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //FuncionesParaDoWhile.MostrarNumeros();
+    }
+}
---
+++ FuncionesParaDoWhile.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplodowhile;
+
+/**
+ *
+ * @author alumno
+ */
+public class FuncionesParaDoWhile {
+    public static void MostrarNumeros()
+    {
+        Integer numero=10;
+        do{
+            System.out.println("numero: "+numero);
+            //tomar dato...numero=
+        }while(numero<10);
+        
+//        System.out.println("mensaje");
+//        tomar dato...numero=
+//        while(numero>10)
+//        {
+//            System.out.println("mensaje");
+//            tomar dato...numero=
+//        }
+    }
+}
